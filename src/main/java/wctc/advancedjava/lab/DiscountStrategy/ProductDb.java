cb8a9cba53c8823c43147d9290f2d2b3c828a03b
rdunckel <rdunckel@my.wctc.edu>
2012-03-04 13:49:15 -0600

    testing //NOSONAR again

--- src/main/java/wctc/advancedjava/lab/DiscountStrategy/ProductDb.java
+++ src/main/java/wctc/advancedjava/lab/DiscountStrategy/ProductDb.java
@@ -22,25 +22,25 @@ public class ProductDb {
 		// PercentageDiscount
 		productList[0] = new Product("A102");
 		productList[0].setDescription("Black Shoes");
-		productList[0].setUnitCost(61.99); //NOSONAR
+		productList[0].setUnitCost(61.99); 
 		productList[0].setDiscountStrategy(new NoDiscount());
 		productList[1] = new Product("B214");
 		productList[1].setDescription("Small Shirt");
-		productList[1].setUnitCost(39.99); //NOSONAR
+		productList[1].setUnitCost(39.99); 
 		productList[1].setDiscountStrategy(new BuyOneGetOneDiscount());
 		productList[2] = new Product("C158");
 		productList[2].setDescription("Khaki Pants");
-		productList[2].setUnitCost(53.60); //NOSONAR
+		productList[2].setUnitCost(53.60); 
 		productList[2].setDiscountStrategy(new QuantityDiscount());
 		productList[3] = new Product("D489");
 		productList[3].setDescription("Pyrex Bowl");
-		productList[3].setUnitCost(10.99); //NOSONAR
+		productList[3].setUnitCost(10.99); 
 		productList[3].setDiscountStrategy(new FlatDiscount());
 		productList[4] = new Product("E567");
 		productList[4].setDescription("Stand Mixer");
-		productList[4].setUnitCost(162.99); //NOSONAR
+		productList[4].setUnitCost(162.99); 
 		productList[4].setDiscountStrategy(new PercentageDiscount());
-	}
+	} //NOSONAR
 
 	public static final Product findProduct(String productId) {
 
