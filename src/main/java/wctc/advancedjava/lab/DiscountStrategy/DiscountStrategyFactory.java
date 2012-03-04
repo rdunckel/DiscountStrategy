63c5d7edbf51dabf3eb667ec075a7d3125790014
rdunckel <rdunckel@my.wctc.edu>
2012-03-04 12:40:49 -0600

    added some javadoc to DiscountStrategyFactory

--- src/main/java/wctc/advancedjava/lab/DiscountStrategy/DiscountStrategyFactory.java
+++ src/main/java/wctc/advancedjava/lab/DiscountStrategy/DiscountStrategyFactory.java
@@ -1,23 +1,28 @@
 package wctc.advancedjava.lab.DiscountStrategy;
 
-
 /**
  * Factory to create DiscountStrategy objects.
- *
+ * 
  * @author Ryan Dunckel
  */
 public class DiscountStrategyFactory {
 
-    private DiscountStrategyFactory instance;
+	private DiscountStrategyFactory instance;
 
-    private DiscountStrategyFactory() {}
+	private DiscountStrategyFactory() {
+	}
 
-    public DiscountStrategyFactory getInstance() {
+	/**
+	 * Get instance of the factory to use for building objects.
+	 * 
+	 * @return
+	 */
+	public DiscountStrategyFactory getInstance() {
 
-        if (instance == null) {
-            instance = new DiscountStrategyFactory();
-        }
-        return instance;
+		if (instance == null) {
+			instance = new DiscountStrategyFactory();
+		}
+		return instance;
 
-    }
+	}
 }
